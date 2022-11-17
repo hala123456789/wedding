b2c3ebe38e59218734c717a57c4a3bed14da35a8
hala123456789 <118438613+hala123456789@users.noreply.github.com>
2022-11-17 12:46:13 +0200

    Add files via upload

---
+++ 2131_wedding_lite/js/magnific-popup-options.js
@@ -0,0 +1,44 @@
+
+
+var typed = new Typed('.auto', {
+  // Waits 1000ms after typing "First"
+  strings: ['First ^1000 sentence.', 'Second sentence.']
+  typeSpeed:50,
+  backSpeed:50,
+});
+
+
+
+
+$(document).ready(function() {
+  // MagnificPopup
+  var magnifPopup = function() {
+    $('.popup-image').magnificPopup({
+      type: 'image',
+      removalDelay: 300,
+      mainClass: 'mfp-with-zoom',
+      gallery:{
+        enabled:true
+      },
+      zoom: {
+        enabled: true, // By default it's false, so don't forget to enable it
+        duration: 300, // duration of the effect, in milliseconds
+        easing: 'ease-in-out', // CSS transition easing function
+                                                                  
+        // The "opener" function should return the element from which popup will be zoomed in
+        // and to which popup will be scaled down
+        // By defailt it looks for an image tag:
+        opener: function(openerElement) {
+        // openerElement is the element on which popup was initialized, in this case its <a> tag
+        // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+        return openerElement.is('img') ? openerElement : openerElement.find('img');
+        }
+      }
+    });
+  };
+  
+  // Call the functions 
+  magnifPopup();
+
+});
+
